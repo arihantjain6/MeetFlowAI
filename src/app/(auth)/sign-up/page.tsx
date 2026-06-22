4490d550984722d0e25285b894229a69ec39ee08
arihantjain6 <arihantjainog@gmail.com>
2026-06-22 14:49:43 +0530

    authentication-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+    console.log("Hello from sign up page");
+    return (
+        <SignUpView />
+    )
+}
+
+export default Page;
