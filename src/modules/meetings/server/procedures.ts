ae0ef1def70932d67027e464dbeb037d0c1b08ab
arihantjain6 <arihantjainog@gmail.com>
2026-08-21 11:58:48 +0530

    meetings data table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,12 +1,12 @@
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import {
   createTRPCRouter,
   protectedProcedure,
 } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import z from "zod";
-import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import {
   DEFAULT_PAGE,
   DEFAULT_PAGE_SIZE,
@@ -81,8 +81,12 @@ export const meetingsRouter = createTRPCRouter({
       const { search, page, pageSize } = input;
 
       const data = await db
-        .select({ ...getTableColumns(meetings) })
+        .select({ ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
@@ -96,6 +100,8 @@ export const meetingsRouter = createTRPCRouter({
       const total = await db
         .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
+
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,34 +1,43 @@
 "use client"
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
-export const MeetingsView =  () => {
+export const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
     return (
-        <div className="overflow-x-scroll">
-            {JSON.stringify(data)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data.items} columns={columns} />
+            {data.items.length === 0 && (
+                <EmptyState
+                    title="No Meetings Yet"
+                    description="It looks like you haven't hosted any meetings with your AI agent yet. Start your first meeting to see your agent in action."
+                />
+            )}
         </div>
     )
 }
 
 export const MeetingViewLoading = () => {
     return (
-        <LoadingState 
-        title="Loading Meetings"
-        description="Please wait while we fetch your meetings..."
+        <LoadingState
+            title="Loading Meetings"
+            description="Please wait while we fetch your meetings..."
         />
     )
-} 
+}
 
 export const MeetingViewError = () => {
     return (
-        <ErrorState 
-        title="Failed to load meetings"
-        description="Please try again later..."
+        <ErrorState
+            title="Failed to load meetings"
+            description="Please try again later..."
         />
     )
 }
